87ad5f2c37d919204eaa52f66cbe99b46fcabf44
iamhaitham <iamhaitham2011@gmail.com>
2024-08-07 14:14:54 +0300

    Implement "IMobileNumberValidator" in "Business" layer using the newly added library

---
+++ Business/Validators/MobileNumberValidator.cs
@@ -0,0 +1,26 @@
+﻿using Core.DTOs;
+using Core.Interfaces;
+using PhoneNumbers;
+
+namespace Business.Validators;
+
+public class MobileNumberValidator : IMobileNumberValidator
+{
+    public bool ValidateMobileNumber(MobileNumberDto mobileNumberDto)
+    {
+        var phoneNumberUtil = PhoneNumberUtil.GetInstance();
+
+        try
+        {
+            var parsedMobileNumber = phoneNumberUtil.Parse(mobileNumberDto.Number, mobileNumberDto.Region);
+
+            return phoneNumberUtil.IsValidNumber(parsedMobileNumber);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+
+            return false;
+        }
+    }
+}
