cbbd9178fd6a1a2289b8aa29bc7d455a8c4e8c2e
iamhaitham <iamhaitham2011@gmail.com>
2024-08-07 13:09:05 +0300

    Create an "Address" entity

---
+++ Infrastructure/Entities/Address.cs
@@ -0,0 +1,12 @@
+﻿namespace Infrastructure.Entities;
+
+public class Address
+{
+    public string Country { get; set; } = string.Empty;
+    
+    public string City { get; set; } = string.Empty;
+    
+    public string Street { get; set; } = string.Empty;
+    
+    public string ZipCode { get; set; } = string.Empty;
+}
