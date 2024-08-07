03b911cac4d0f3de401f8c391e63a822c161443e
iamhaitham <iamhaitham2011@gmail.com>
2024-08-07 15:28:52 +0300

    Create "IUserRepository" with an implementation

---
+++ Infrastructure/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using Infrastructure.Entities;
+
+namespace Infrastructure.Repositories.Interfaces;
+
+public interface IUserRepository
+{
+    public void Create(User user);
+}
---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Infrastructure.Entities;
+using Infrastructure.Repositories.Interfaces;
+
+namespace Infrastructure.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    public void Create(User user)
+    {
+        throw new NotImplementedException();
+    }
+}
