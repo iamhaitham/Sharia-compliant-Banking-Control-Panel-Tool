347068738cfe0704c385ff74f88bbd2955b6bd25
iamhaitham <iamhaitham2011@gmail.com>
2024-08-07 17:46:20 +0300

    Create "AppDbContext"

---
+++ Infrastructure/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Address> Addresses { get; set; }
+
+    public DbSet<User> Users { get; set; }
+    
+    public DbSet<Client> Clients { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
