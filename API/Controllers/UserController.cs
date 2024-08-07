3ac1a85ab92d9fabab25e1dc2aed4e5ef6b05194
iamhaitham <iamhaitham2011@gmail.com>
2024-08-07 18:21:41 +0300

    Create "UserController" and add a method to add users

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+using System.Net;
+using Business.Services.Interfaces;
+using Core.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost("register")]
+    [ProducesResponseType((int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+    public async Task<ActionResult<RegisterUserResponseDto>> Register(RegisterUserRequestDto registerUserRequestDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest();
+        }
+
+        var registerUserResponseDto = await _userService.Register(registerUserRequestDto);
+
+        return Ok(registerUserResponseDto);
+    }
+}
