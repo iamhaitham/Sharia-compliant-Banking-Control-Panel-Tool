2a37e837daff7e1e8a1eaf9373f77185a84456f9
iamhaitham <iamhaitham2011@gmail.com>
2024-08-07 13:25:29 +0300

    Create a "CustomErrorMessage" utility class

---
+++ Core/Utilities/CustomErrorMessage.cs
@@ -0,0 +1,22 @@
+﻿namespace Core.Utilities;
+
+public static class CustomErrorMessage
+{
+    public static string IncompatibleTypeMessage(string typeName) => 
+        $"Value provided must be a {typeName}";
+    
+    public static string InvalidMobileNumberFormat() =>
+        "Invalid mobile number format";
+
+    public static string ExactStringLengthNotSatisfied(int exactStringLength) =>
+        $"An exact string length of {exactStringLength} was not satisfied";
+
+    public static string CouldNotResolveDependency(string dependencyName) =>
+        $"Could not resolve dependency {dependencyName}";
+    
+    public static string CouldNotResolveType(string typeName) =>
+        $"Could not resolve type {typeName}";
+
+    public static string CannotBeNull(string propertyName) =>
+        $"{propertyName} cannot be null";
+}
