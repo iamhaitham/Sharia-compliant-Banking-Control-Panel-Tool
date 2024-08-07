687398438a0ff9b843bd1c5c84bec1da5e948808
iamhaitham <iamhaitham2011@gmail.com>
2024-08-07 14:15:16 +0300

    Create "MobileNumberAttribute" that verifies the mobile number format

---
+++ Core/CustomAttributes/MobileNumberAttribute.cs
@@ -0,0 +1,39 @@
+using System.ComponentModel.DataAnnotations;
+using Core.DTOs;
+using Core.Interfaces;
+using Core.Utilities;
+
+namespace Core.CustomAttributes;
+
+[AttributeUsage(AttributeTargets.Property)]
+public sealed class MobileNumberAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is not MobileNumberDto)
+        {
+            return new ValidationResult(
+                CustomErrorMessage.IncompatibleTypeMessage(
+                    nameof(MobileNumberDto)
+                )
+            );
+        }
+
+        var mobileNumberValidator = (IMobileNumberValidator?)validationContext.GetService(typeof(IMobileNumberValidator));
+
+        if (mobileNumberValidator is null)
+        {
+            return new ValidationResult(
+                CustomErrorMessage.CouldNotResolveDependency(
+                    nameof(IMobileNumberValidator)
+                )
+            );
+        }
+
+        return mobileNumberValidator.ValidateMobileNumber((MobileNumberDto)value)
+            ? ValidationResult.Success
+            : new ValidationResult(
+                CustomErrorMessage.InvalidMobileNumberFormat()
+            );
+    }
+}
