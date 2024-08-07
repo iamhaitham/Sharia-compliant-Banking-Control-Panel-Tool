8670b9788c49f168f43e2c9037d6f6385807d926
iamhaitham <iamhaitham2011@gmail.com>
2024-08-07 15:40:37 +0300

    Add Id property for Address and Person entities

--- Infrastructure/Entities/Address.cs
+++ Infrastructure/Entities/Address.cs
@@ -2,6 +2,8 @@
 
 public class Address
 {
+    public int Id { get; set; }
+
     public string Country { get; set; } = string.Empty;
     
     public string City { get; set; } = string.Empty;
--- Infrastructure/Entities/Person.cs
+++ Infrastructure/Entities/Person.cs
@@ -4,6 +4,8 @@ namespace Infrastructure.Entities;
 
 public class Person
 {
+    public int Id { get; set; }
+
     public required string FirstName { get; set; }
 
     public required string LastName { get; set; }
