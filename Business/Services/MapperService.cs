f52d6971a60746c9fe494661258a9c4378b064d6
iamhaitham <iamhaitham2011@gmail.com>
2024-08-07 15:29:23 +0300

    Create "MapperService" with some mapping methods

---
+++ Business/Services/MapperService.cs
@@ -0,0 +1,46 @@
+﻿using Core.DTOs;
+using Infrastructure.Entities;
+
+namespace Business.Services;
+
+public class MapperService
+{
+    public static RegisterUserRequestDto MapMapRegisterUserRequestDtoToACopy(RegisterUserRequestDto registerUserRequestDto)
+    {
+        return new RegisterUserRequestDto()
+        {
+            Email = registerUserRequestDto.Email,
+            Password = registerUserRequestDto.Password,
+            Role = registerUserRequestDto.Role,
+            Sex = registerUserRequestDto.Sex,
+            FirstName = registerUserRequestDto.FirstName,
+            LastName = registerUserRequestDto.LastName,
+            MobileNumber = registerUserRequestDto.MobileNumber,
+            PersonalId = registerUserRequestDto.PersonalId
+        };
+    }
+
+    public static User MapRegisterUserRequestDtoToUser(RegisterUserRequestDto registerUserRequestDto)
+    {
+        return new User()
+        {
+            Email = registerUserRequestDto.Email,
+            PasswordHash = registerUserRequestDto.Password,
+            Role = registerUserRequestDto.Role,
+            Sex = registerUserRequestDto.Sex,
+            FirstName = registerUserRequestDto.FirstName,
+            LastName = registerUserRequestDto.LastName,
+            MobileNumber = registerUserRequestDto.MobileNumber.Number,
+            PersonalId = registerUserRequestDto.PersonalId
+        };
+    }
+
+    public static RegisterUserResponseDto MapUserToRegisterUserResponseDto(User user)
+    {
+        return new RegisterUserResponseDto()
+        {
+            Email = user.Email,
+            PasswordHash = user.PasswordHash
+        };
+    }
+}
