6536388c976cf0b8a6c56442958c474891224ef6
iamhaitham <iamhaitham2011@gmail.com>
2024-08-07 15:05:22 +0300

    Rename a property in User

--- Infrastructure/Entities/User.cs
+++ Infrastructure/Entities/User.cs
@@ -4,7 +4,7 @@ namespace Infrastructure.Entities;
 
 public class User : Person
 {
-    public required string Password { get; set; }
+    public required string PasswordHash { get; set; }
     
     public required UserRole Role { get; set; }
 }
