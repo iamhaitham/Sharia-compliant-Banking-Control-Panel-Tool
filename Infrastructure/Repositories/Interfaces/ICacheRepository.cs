9b9985df5e8ff75eaa25a85e5ff2252a2ecd1e04
iamhaitham <iamhaitham2011@gmail.com>
2024-08-08 23:13:44 +0300

    Create "ICacheRepository"

---
+++ Infrastructure/Repositories/Interfaces/ICacheRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Infrastructure.Repositories.Interfaces;
+
+public interface ICacheRepository
+{
+    public Task<Queue<T>> GetAsync<T>(string key, CancellationToken cancellationToken = default)
+        where T : class;
+    
+    public Task SetAsync<T>(string key, T value, CancellationToken cancellationToken = default)
+        where T : class;
+}
