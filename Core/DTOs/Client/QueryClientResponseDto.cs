40c503c3e7d1c76c3943466225bc38b647a0480f
iamhaitham <iamhaitham2011@gmail.com>
2024-08-08 19:51:58 +0300

    Create "QueryClientResponseDto"

---
+++ Core/DTOs/Client/QueryClientResponseDto.cs
@@ -0,0 +1,23 @@
+﻿using Core.DTOs.Address;
+using Core.Enums;
+
+namespace Core.DTOs.Client;
+
+public class QueryClientResponseDto
+{
+    public required string FirstName { get; set; }
+
+    public required string LastName { get; set; }
+    
+    public Sex? Sex { get; set; }
+    
+    public AddressDto Address { get; set; } = new();
+    
+    public required string MobileNumber { get; set; }
+    
+    public required string Email { get; set; }
+    
+    public required string PersonalId { get; set; }
+    
+    public required List<string> Accounts { get; set; }
+}
