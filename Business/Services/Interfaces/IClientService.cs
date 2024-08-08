b7109ed75dbfdfd0427379b56ff9c6251a69ddf4
iamhaitham <iamhaitham2011@gmail.com>
2024-08-08 19:50:41 +0300

    Add "Query" method to "IClientService"

--- Business/Services/Interfaces/IClientService.cs
+++ Business/Services/Interfaces/IClientService.cs
@@ -6,4 +6,6 @@ namespace Business.Services.Interfaces;
 public interface IClientService
 {
     public Task<GenericResponse<RegisterClientResponseDto>> Register(RegisterClientRequestDto registerClientRequestDto);
+
+    public Task<GenericResponse<List<QueryClientResponseDto>>> Query(QueryClientRequestDto queryClientRequestDto);
 }
