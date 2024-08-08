7800ee8b6276cec10dcbdf23e94d54d9c7f220ef
iamhaitham <iamhaitham2011@gmail.com>
2024-08-08 15:03:42 +0300

    Implement "IAddressRepository"

---
+++ Infrastructure/Repositories/AddressRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq.Expressions;
+using Infrastructure.Entities;
+using Infrastructure.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class AddressRepository : IAddressRepository
+{
+    private readonly AppDbContext _dbContext;
+    private readonly DbSet<Address> _dbSet;
+
+    public AddressRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _dbSet = _dbContext.Set<Address>();
+    }
+
+    public async Task Create(Address address)
+    {
+        await _dbSet.AddAsync(address);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<Address?> GetByFilter(Expression<Func<Address, bool>> filter)
+    {
+        return await _dbSet.FirstOrDefaultAsync(filter);
+    }
+}
