e6c3a5f5d62cd9359584225c1b8754cd564a339b
iamhaitham <iamhaitham2011@gmail.com>
2024-08-08 15:06:19 +0300

    Update the logic of client registration to not duplicate Address entities in case they are identical

--- Business/Services/ClientService.cs
+++ Business/Services/ClientService.cs
@@ -10,20 +10,27 @@ public class ClientService : IClientService
 {
     private readonly IClientRepository _clientRepository;
     private readonly IClientValidator _clientValidator;
+    private readonly IAddressRepository _addressRepository;
+    private readonly IAddressValidator _addressValidator;
 
     public ClientService(
         IClientRepository clientRepository,
-        IClientValidator clientValidator
+        IClientValidator clientValidator,
+        IAddressRepository addressRepository,
+        IAddressValidator addressValidator
     )
     {
         _clientRepository = clientRepository;
         _clientValidator = clientValidator;
+        _addressRepository = addressRepository;
+        _addressValidator = addressValidator;
     }
 
     public async Task<ResponseDto<RegisterClientResponseDto>> Register(
         RegisterClientRequestDto registerClientRequestDto
     )
     {
+        // If client is already registered, stop the registration process.
         var isClientUniqueResponse = await _clientValidator.IsUnique(registerClientRequestDto);
 
         if (!isClientUniqueResponse.IsSuccessful)
@@ -33,6 +40,45 @@ public class ClientService : IClientService
         
         var client = MapperService.MapRegisterClientRequestDtoToClient(registerClientRequestDto);
 
+        // Validate whether the address already exists or not, and only create it if it does not exist yet (unique).
+        var isAddressUniqueResponse = await _addressValidator.IsUnique(registerClientRequestDto.Address);
+
+        if (!isAddressUniqueResponse.IsSuccessful)
+        {
+            return new ResponseDto<RegisterClientResponseDto>()
+            {
+                Errors = isAddressUniqueResponse.Errors,
+                IsSuccessful = isAddressUniqueResponse.IsSuccessful,
+                HttpCode = isAddressUniqueResponse.HttpCode
+            };
+        }
+
+        if (isAddressUniqueResponse.HttpCode == HttpStatusCode.Conflict)
+        {
+            client.Address = isAddressUniqueResponse.Body!;
+        }
+        else
+        {
+            try
+            {
+                await _addressRepository.Create(
+                    client.Address
+                );
+            }
+            catch (Exception ex)
+            {
+                return new ResponseDto<RegisterClientResponseDto>()
+                {
+                    Errors = new List<string>()
+                    {
+                        ex.Message
+                    },
+                    IsSuccessful = false,
+                    HttpCode = HttpStatusCode.InternalServerError
+                };
+            }
+        }
+        
         try
         {
             await _clientRepository.Create(client);
