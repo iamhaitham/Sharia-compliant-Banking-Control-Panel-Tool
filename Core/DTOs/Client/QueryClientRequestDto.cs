b9bdb8c78f753f5c29af581736e0e94d3b5af216
iamhaitham <iamhaitham2011@gmail.com>
2024-08-08 19:51:47 +0300

    Create "QueryClientRequestDto"

---
+++ Core/DTOs/Client/QueryClientRequestDto.cs
@@ -0,0 +1,34 @@
+﻿using Core.Enums;
+
+namespace Core.DTOs.Client;
+
+public class QueryClientRequestDto
+{
+    public int? Skip { get; set; }
+
+    public int? Take { get; set; }
+
+    public Sort Sort { get; set; } = Sort.Ascending;
+    
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+    
+    public Sex? Sex { get; set; }
+    
+    public string Country { get; set; } = string.Empty;
+    
+    public string City { get; set; } = string.Empty;
+    
+    public string Street { get; set; } = string.Empty;
+    
+    public string ZipCode { get; set; } = string.Empty;
+    
+    public string MobileNumber { get; set; } = string.Empty;
+    
+    public string Email { get; set; } = string.Empty;
+    
+    public string PersonalId { get; set; } = string.Empty;
+
+    public List<string> Accounts { get; set; } = new();
+}
