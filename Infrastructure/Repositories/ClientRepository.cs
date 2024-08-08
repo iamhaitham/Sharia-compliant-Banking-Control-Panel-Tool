5bc35a33d1e2b4e50b07b9b6b56038eb38483a85
iamhaitham <iamhaitham2011@gmail.com>
2024-08-08 19:56:39 +0300

    Implement the filter logic inside "Query" method of "ClientRepository"

--- Infrastructure/Repositories/ClientRepository.cs
+++ Infrastructure/Repositories/ClientRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq.Expressions;
+using Core.DTOs.Client;
 using Infrastructure.Entities;
 using Infrastructure.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -25,5 +26,80 @@ public class ClientRepository : IClientRepository
     public async Task<Client?> GetByFilter(Expression<Func<Client, bool>> filter)
     {
         return await _dbSet.FirstOrDefaultAsync(filter);
+    }
+
+    public async Task<List<Client>> Query(QueryClientRequestDto queryClientRequestDto)
+    {
+        IQueryable<Client> query = _dbSet.Include(c => c.Address);
+        
+        if (!string.IsNullOrWhiteSpace(queryClientRequestDto.FirstName))
+        {
+            query = query.Where(c => c.FirstName.ToLower() == queryClientRequestDto.FirstName.ToLower());
+        }
+        
+        if (!string.IsNullOrWhiteSpace(queryClientRequestDto.LastName))
+        {
+            query = query.Where(c => c.LastName.ToLower() == queryClientRequestDto.LastName.ToLower());
+        }
+        
+        if (queryClientRequestDto.Sex is not null)
+        {
+            query = query.Where(c => c.Sex == queryClientRequestDto.Sex);
+        }
+        
+        if (!string.IsNullOrWhiteSpace(queryClientRequestDto.Country))
+        {
+            query = query.Where(c => c.Address.Country == queryClientRequestDto.Country);
+        }
+        
+        if (!string.IsNullOrWhiteSpace(queryClientRequestDto.City))
+        {
+            query = query.Where(c => c.Address.City == queryClientRequestDto.City);
+        }
+        
+        if (!string.IsNullOrWhiteSpace(queryClientRequestDto.Street))
+        {
+            query = query.Where(c => c.Address.Street == queryClientRequestDto.Street);
+        }
+        
+        if (!string.IsNullOrWhiteSpace(queryClientRequestDto.ZipCode))
+        {
+            query = query.Where(c => c.Address.ZipCode == queryClientRequestDto.ZipCode);
+        }
+        
+        if (!string.IsNullOrWhiteSpace(queryClientRequestDto.MobileNumber))
+        {
+            query = query.Where(c => c.MobileNumber == queryClientRequestDto.MobileNumber);
+        }
+        
+        if (!string.IsNullOrWhiteSpace(queryClientRequestDto.Email))
+        {
+            query = query.Where(c => c.Email == queryClientRequestDto.Email);
+        }
+        
+        if (!string.IsNullOrWhiteSpace(queryClientRequestDto.PersonalId))
+        {
+            query = query.Where(c => c.PersonalId == queryClientRequestDto.PersonalId);
+        }
+        
+        if (queryClientRequestDto.Accounts.Count != 0)
+        {
+            foreach (var account in queryClientRequestDto.Accounts)
+            {
+                query = query.Where(c => c.Accounts.Any(a => a == account));
+            }
+        }
+
+        if (queryClientRequestDto.Skip is not null && queryClientRequestDto.Skip != 0)
+        {
+            query = query.Skip((int)queryClientRequestDto.Skip);
+        }
+        
+        if (queryClientRequestDto.Take is not null && queryClientRequestDto.Take != 0)
+        {
+            query = query.Take((int)queryClientRequestDto.Take);
+        }
+        
+        return await query.ToListAsync();
     }
 }
