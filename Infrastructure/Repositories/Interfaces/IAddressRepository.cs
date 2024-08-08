5d4013357242194ad2ce4d8b057b0d432c979cc3
iamhaitham <iamhaitham2011@gmail.com>
2024-08-08 15:03:32 +0300

    Create "IAddressRepository"

---
+++ Infrastructure/Repositories/Interfaces/IAddressRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+using Infrastructure.Entities;
+
+namespace Infrastructure.Repositories.Interfaces;
+
+public interface IAddressRepository
+{
+    public Task Create(Address address);
+
+    public Task<Address?> GetByFilter(Expression<Func<Address, bool>> filter);
+}
